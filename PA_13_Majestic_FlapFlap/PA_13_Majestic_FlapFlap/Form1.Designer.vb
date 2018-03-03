a2c891f051192d8d3e2a79598a6c88f089be8adf
Kevin Jordan <kevincentdeco@gmail.com>
2018-03-03 15:05:12 +0700

    test push

--- PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.Designer.vb
+++ PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.Designer.vb
@@ -72,7 +72,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(636, 111)
+        Me.Button1.Location = New System.Drawing.Point(639, 99)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 4
