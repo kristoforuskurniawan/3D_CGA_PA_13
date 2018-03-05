976c4b2c4853c553dc043c3da3ad5f158fdd4d7c
kristoforuskurniawan <kristoforus10@yahoo.com>
2018-03-05 20:03:42 +0700

    Revert from using class. Implement polymesh with built-in library.

--- PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.vb
+++ PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.vb
@@ -1,5 +1,36 @@
 ﻿Public Class Form1
+
+    Private bitmapCanvas As Bitmap
+    Private graphics As Graphics
+    'Private Vertices As TPoint = Nothing
+    'Private Edges As TLine = Nothing
+    'Private Polygon As TPolygon = Nothing
+    'Private Cuboid_Surface As TSurface = Nothing
+    'Private Cuboid As PolygonMesh_Cuboid = Nothing
+
+    Private CubeVertices As List(Of Point) 'Pakai built-in List(Of T) biar ga ribet. Fok off class
+    Private CubeEdges As List(Of Integer) 'Index of CubeVertices
+    Private Poly As List(Of Integer) 'Index of CubeEdges
+    Private Surface As List(Of Integer) 'Index of Poly
+    Private CuboidShape As List(Of Integer) 'Index of Surface
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Vertices = Nothing
+        'Edges = Nothing
+        'Polygon = Nothing
+        'Cuboid_Surface = Nothing
+        'Cuboid = Nothing
+
+        bitmapCanvas = New Bitmap(MainCanvas.Width, MainCanvas.Height)
+        graphics = CreateGraphics()
+        graphics = Graphics.FromImage(bitmapCanvas)
+        MainCanvas.Image = bitmapCanvas
+
+        CubeVertices = New List(Of Point)
+        CubeEdges = New List(Of Integer)
+        Poly = New List(Of Integer)
+        Surface = New List(Of Integer)
+        CuboidShape = New List(Of Integer)
 
     End Sub
 
@@ -7,4 +38,21 @@
         CoordinatesLabel.Text = "Coordinates: X = " + e.X.ToString() + ", Y = " + e.Y.ToString()
     End Sub
 
+    Private Sub MatrixMultiplication_4x4(Matrix_A()()()() As Integer, Matrix_B()()()() As Integer) 'Is this required or is there any built-in library for 4x4 multiplication matrix?
+
+    End Sub
+
+    Private Sub MainCanvas_Click(sender As Object, e As MouseEventArgs) Handles MainCanvas.Click
+        CubeVertices.Add(New Point(e.X, e.Y))
+        graphics.DrawEllipse(Pens.Black, e.X, e.Y, 10, 10)
+        MainCanvas.Image = bitmapCanvas
+        'For i = 0 To CubeVertices.Count - 1
+        '    MessageBox.Show(CubeVertices(i).X & ", " & CubeVertices(i).Y)
+        'Next
+        'Vertices = Insert(Vertices, e.X, e.Y, 0)
+        'While (Vertices IsNot Nothing)
+        '    MessageBox.Show(Vertices.X & " with index " & Vertices.PointIndex.ToString())
+        '    Vertices = Vertices.NextPoint
+        'End While
+    End Sub
 End Class
--- PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/TPoint.vb
+++ PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/TPoint.vb
@@ -1,3 +1,41 @@
 ﻿Public Class TPoint
-    Public X, Y, Z, PointIndex As Integer 'Store the x,y and z coord of this vertex as well as its index number
+
+    Public X, Y, Z As Integer 'Store the x,y and z coord of this vertex as well as its index number
+    Public NextPoint As TPoint
+    Public PointIndex As Integer = -1
+
+    Public Sub New()
+        Me.X = 0
+        Me.Y = 0
+        Me.Z = 0
+        Me.PointIndex = 0
+        Me.NextPoint = Nothing
+    End Sub
+
+    Public Sub New(X As Integer, Y As Integer, Z As Integer)
+        Me.X = X
+        Me.Y = Y
+        Me.Z = Z
+        Me.PointIndex = 0
+        Me.NextPoint = Nothing
+    End Sub
+
+    Public Sub SetCoordinates(ByVal X As Integer, ByVal Y As Integer, ByVal Z As Integer) 'Method to set coordinates
+        Me.X = X
+        Me.Y = Y
+        Me.Z = Z
+    End Sub
+
+    'Public Function Insert(ByRef InsertedPoint As TPoint, ByVal X As Integer, ByVal Y As Integer, ByVal Z As Integer) As TPoint 'Method to insert new point
+    '    If (InsertedPoint Is Nothing) Then
+    '        InsertedPoint = New TPoint()
+    '        InsertedPoint.PointIndex = 1
+    '        InsertedPoint.SetCoordinates(X, Y, Z)
+    '    Else
+    '        InsertedPoint.PointIndex = InsertedPoint.PointIndex + 1
+    '        Insert(InsertedPoint.NextPoint, X, Y, Z)
+    '    End If
+    '    Return InsertedPoint
+    'End Function
+
 End Class
