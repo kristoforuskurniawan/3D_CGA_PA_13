63fdb7eaa335b9dcd217910244a00a90698c3c3b
ARDYWIJAYA <ardywijaya1997@gmail.com>
2018-03-07 17:33:47 +0700

    test

--- PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.Designer.vb
+++ PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.Designer.vb
@@ -27,6 +27,7 @@ Partial Class Form1
         Me.WalkRadioButton = New System.Windows.Forms.RadioButton()
         Me.CoordinatesLabel = New System.Windows.Forms.Label()
         Me.DrawChickenButton = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.MainCanvas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -79,11 +80,21 @@ Partial Class Form1
         Me.DrawChickenButton.Text = "Draw Chicken"
         Me.DrawChickenButton.UseVisualStyleBackColor = True
         '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(639, 98)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(806, 508)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.DrawChickenButton)
         Me.Controls.Add(Me.CoordinatesLabel)
         Me.Controls.Add(Me.WalkRadioButton)
@@ -102,4 +113,5 @@ Partial Class Form1
     Friend WithEvents WalkRadioButton As RadioButton
     Friend WithEvents CoordinatesLabel As Label
     Friend WithEvents DrawChickenButton As Button
+    Friend WithEvents Button2 As Button
 End Class
