5b44290671dd8a65122e823fa7d5105bb1a33522
ARDYWIJAYA <ardywijaya1997@gmail.com>
2018-03-07 16:58:13 +0700

    test

--- PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.Designer.vb
+++ PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.Designer.vb
@@ -28,6 +28,7 @@ Partial Class Form1
         Me.CoordinatesLabel = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
         CType(Me.MainCanvas, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -89,11 +90,21 @@ Partial Class Form1
         Me.Button2.Text = "Button2"
         Me.Button2.UseVisualStyleBackColor = True
         '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(639, 141)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(806, 508)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.CoordinatesLabel)
@@ -114,4 +125,5 @@ Partial Class Form1
     Friend WithEvents CoordinatesLabel As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
