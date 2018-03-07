dc9db67e59a6944d63be3082367cbb5416291c0b
kristoforuskurniawan <kristoforus10@yahoo.com>
2018-03-07 10:13:18 +0700

    Trying to Draw Cuboid (No polygon mesh)

--- PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.vb
+++ PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
 
+    'Gimana kalau deklarasi object di Form1 cukup punya Cuboid aja? Sisanya di dalam class cuboid
+
     Private BitmapCanvas As Bitmap
     Private G As Graphics
     Private Vertices As TPoint
@@ -8,6 +10,9 @@
     Private CubeSurface As TSurface
     Private Cuboid As PolygonMesh_Cuboid
     Private M(,), Wt(,), Vt(,), St(,) As Double
+
+    'Private Test_CubePoint As TPoint
+    Private Test_CubeLine(12) As TLine
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Vertices = New TPoint()
@@ -25,6 +30,9 @@
         Wt = New Double(4, 4) {}
         Vt = New Double(4, 4) {}
         St = New Double(4, 4) {}
+
+        'Test_CubePoint = New List(Of TPoint)
+        Test_CubeLine = New TLine(11) {}
     End Sub
 
     Private Sub MainCanvas_MouseOver(sender As Object, e As MouseEventArgs) Handles MainCanvas.MouseMove
@@ -37,6 +45,54 @@
 
     Private Sub declare_all_object()
 
+    End Sub
+
+    'Private Sub Perspective(x As Double, y As Double, z As Double)
+    '    If x = 0 Then
+    '        x = 0
+    '    Else
+    '        x = 1.0 / x
+    '    End If
+    '    If y = 0 Then
+    '        y = 0
+    '    Else
+    '        y = 1.0 / y
+    '    End If
+    '    If z = 0 Then
+    '        z = 0
+    '    Else
+    '        z = 1.0 / z
+    '    End If
+    '    SetColMat(Trans, 0, 1, 0, 0, 0)
+    '    SetColMat(Trans, 1, 0, 1, 0, 0)
+    '    SetColMat(Trans, 2, 0, 0, 1, 0)
+    '    SetColMat(Trans, 3, x, y, z, 1)
+    'End Sub
+
+    'Sub InitCube()
+    '    SetPoint(V(0), -1, -1, 1)
+    '    SetPoint(V(1), 1, -1, 1)
+    '    SetPoint(V(2), 1, 1, 1)
+    '    SetPoint(V(3), -1, 1, 1)
+    '    SetPoint(V(4), -1, -1, -1)
+    '    SetPoint(V(5), 1, -1, -1)
+    '    SetPoint(V(6), 1, 1, -1)
+    '    SetPoint(V(7), -1, 1, -1)
+    'End Sub
+
+    'Sub DrawCube()
+    '    Dim i, j As Integer
+    '    For j = 0 To 3
+    '        G.DrawLine(Pens.Red, VS(Edge(j).p1).x, VS(Edge(j).p1).y, VS(Edge(j).p2).x, VS(Edge(j).p2).y)
+    '    Next
+    '    For i = 4 To 11
+    '        G.DrawLine(Pens.Black, VS(Edge(i).p1).x, VS(Edge(i).p1).y, VS(Edge(i).p2).x, VS(Edge(i).p2).y)
+    '    Next
+    '    MainCanvas.Image = BitmapCanvas
+    'End Sub
+
+    Private Sub DrawChickenButton_Click(sender As Object, e As EventArgs) Handles DrawChickenButton.Click
+        'DrawCube()
     End Sub
 
     Function MultiplyMat(point As TPoint, M(,) As Double) As TPoint
@@ -54,17 +110,20 @@
         ' P' = P.Wt.Vt.St
         ' P -> object
         ' P'-> projection
-        Dim Wt(4, 4) As Double 'World
+
+        'World
         FillRow(0, 1, 0, 0, 0, Wt)
         FillRow(1, 0, 1, 0, 0, Wt)
         FillRow(2, 0, 0, 1, 0, Wt)
         FillRow(3, 0, 0, 0, 1, Wt)
-        Dim Vt(4, 4) As Double 'Vt
+
+        'Vt
         FillRow(0, 1, 0, 0, 0, Vt)
         FillRow(1, 0, 1, 0, 0, Vt)
         FillRow(2, 0, 0, 0, 0, Vt)
         FillRow(3, 0, 0, 0, 1, Vt)
-        Dim St(4, 4) As Double 'St
+
+        'St
         FillRow(0, 50, 0, 0, 300, St)
         FillRow(1, 0, -50, 0, 180, St)
         FillRow(2, 0, 0, 0, 0, St)
