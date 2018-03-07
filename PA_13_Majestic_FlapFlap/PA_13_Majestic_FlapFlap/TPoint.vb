34ad6ded862287d8fda93ae00bfebd89d7b8c25f
kristoforuskurniawan <kristoforus10@yahoo.com>
2018-03-07 10:20:12 +0700

    Trying to use transformation matrix and draw the torso.

--- PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/TPoint.vb
+++ PA_13_Majestic_FlapFlap/PA_13_Majestic_FlapFlap/TPoint.vb
@@ -1,10 +1,11 @@
 ﻿Public Class TPoint
-    Public X, Y, Z, W As Integer 'Store the x,y and z coord of this vertex as well as its index number
+    Public X, Y, Z As Double 'Store the x,y and z coord of this vertex as well as its index number
+    Public W As Integer
 
     Public Sub New()
-        X = 0
-        Y = 0
-        Z = 0
+        X = 0.00
+        Y = 0.00
+        Z = 0.00
         W = 1
     End Sub
 
